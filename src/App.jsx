dd3662a147146b1937d1f9600d1b149315f294c4
chicongmindx <ngchicong868@gmail.com>
2024-09-08 10:45:08 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,66 @@
-import { useState } from "react";
-import "./app.scss";
-
-import axios from "axios";
-import { Box, Typography } from "@mui/material";
+import React, { useState } from 'react'
+import axios from 'axios'
 
 function App() {
-  const [data, setData] = useState({});
-  const [location, setLocation] = useState("");
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
 
-  const API_KEY = "fceb739b11cfed510276baf19fbd0937";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`
 
-  function searchLocation(event) {
-    if (event.key === "Enter") {
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
       axios.get(url).then((response) => {
-        setData(response.data);
-        setLocation("");
-      });
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
     }
   }
 
   return (
-    <Box className="app">
+    <div className="app">
       <div className="search">
         <input
-          type="text"
           value={location}
-          onChange={(event) => setLocation(event.target.value)}
-          onKeyDown={searchLocation}
-          placeholder="E.g Hanoi, London"
-        />
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter Location'
+          type="text" />
       </div>
-
       <div className="container">
         <div className="top">
           <div className="location">
-            <Typography variant="h4">SkyForecast</Typography>
-          </div>
-
-          <div className="location">
-            <Typography variant="h4">{data.name}</Typography>
+            <p>{data.name}</p>
           </div>
           <div className="temp">
-            <Typography variant="h2" component={"h2"}>
-              {data.main ? data.main.temp : null}&#176;F
-            </Typography>
+            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
           </div>
           <div className="description">
-            <p>{data.weather[0].main}</p>
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
-        <div className="bottom">
-          <div className="humidity">
-            <Typography variant="h2" component={"h2"}>
-              {data.main ? data.main.humidity : null}%
-            </Typography>
-          </div>
 
-          <div className="wind">
-            <Typography variant="h2" component={"h2"}>
-              {data.wind ? data.wind.speed : null}km/h
-            </Typography>
+        {data.name !== undefined &&
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
           </div>
-        </div>
+        }
+
+
+
       </div>
-    </Box>
+    </div>
   );
 }
 
